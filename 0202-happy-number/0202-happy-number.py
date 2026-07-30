443fc3b02021d46e750b21f51837e59d2d991251
Dharshan <yadavdharshan920@gmail.com>
2026-07-30 12:03:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.69%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,9 +2,7 @@ class Solution(object):
     def isHappy(self, n):
         seen = set()
         last_digit=0
-        while n!=1:
-            if n in seen:
-                return False
+        while n!=1 and n not in seen:
             seen.add(n)
             sum1=0
             while n!=0:
@@ -12,7 +10,7 @@ class Solution(object):
                 sum1=sum1+last_digit**2
                 n=n//10
             n=sum1
-        return True
+        return n==1
        
         
             
