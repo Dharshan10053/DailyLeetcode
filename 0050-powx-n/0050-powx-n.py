ebdf44b28f90d3674093df221a2afb4458577804
Dharshan <yadavdharshan920@gmail.com>
2026-08-03 15:00:16 +0530

    Time: 3 ms (5.52%), Space: 12.3 MB (93.09%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        y=x**n
+        return round(y,5)
+        
